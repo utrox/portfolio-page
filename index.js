11267ccb3add2732c30028da5535677f15264086
utrox <89227600+utrox@users.noreply.github.com>
2022-03-11 20:51:30 +0100

    Design Skills, Projects, Contact section.

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const animatedElements = document.querySelectorAll(".animate-on-scroll");
-const animationScreenProportion = window.innerHeight * 0.8
-const skills = document.querySelectorAll(".single-skill")
-const skillsTransitionDelay = 0.1
-const projectGifs = document.querySelectorAll(".project-gif"); 
-
-
+const animationScreenProportion = 0.95;
+const skills = document.querySelectorAll(".single-skill");
+const skillsTransitionDelay = 0.15;
+const projectGifs = document.querySelectorAll(".project-gif");
 
 skills.forEach((element, index) => {
   element.style.transitionDelay = `${index * skillsTransitionDelay}s`;
-})
+});
 
 function handleScroll() {
   animatedElements.forEach((element) => {
     const distanceFromTop = element.getBoundingClientRect().top;
-    if (distanceFromTop - animationScreenProportion < 0) {
+    if (distanceFromTop - window.innerHeight * animationScreenProportion < 0) {
       element.classList.add("active");
     } else {
       element.classList.remove("active");
@@ -22,20 +20,19 @@ function handleScroll() {
 }
 
 function toggleActive() {
-  skills.forEach(element => {
-    element.classList.toggle("active")
-  })
+  skills.forEach((element) => {
+    element.classList.toggle("active");
+  });
 }
 
 document.addEventListener("scroll", handleScroll);
 
-
 projectGifs.forEach((gif) => {
   gif.addEventListener("mouseenter", () => {
-    gif.src = gif.dataset.gifPath
-  })
+    gif.src = gif.dataset.gifPath;
+  });
 
   gif.addEventListener("mouseleave", () => {
     gif.src = gif.dataset.thumbnailPath;
   });
-})
+});
---
+++ projects.js
@@ -0,0 +1,15 @@
+const html = "fa-html5";
+const css = "fa-css3-alt";
+const react = "fa-react";
+const node = "fa-node";
+
+const projects = [
+  {
+    title: "Name and title of the project",
+    description:
+      "Description of the project. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa nisi, odio voluptates illum est vitae doloremque nostrum blanditiis laborum, repellendus laudantium sed quis nobis modi iusto alias dolorum magnam provident?",
+    madeWith: ["css", "html", "react", "node"],
+    github: "https://www.github.com",
+    live: "https://www.google.com",
+  },
+];
